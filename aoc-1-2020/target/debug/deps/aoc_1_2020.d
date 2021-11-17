5c5dbbe050e12e31a0e03b2a57cd4382b1803d03
heroichornet <schnipper@gmail.com>
2021-11-17 22:56:26 +0100

    adding advent of code 2020 challenge 1 solution

---
+++ aoc-1-2020/target/debug/deps/aoc_1_2020.d
@@ -0,0 +1,5 @@
+/Volumes/data/4_github/learn-rust/100-rust-snippets/aoc-1-2020/target/debug/deps/aoc_1_2020: src/main.rs
+
+/Volumes/data/4_github/learn-rust/100-rust-snippets/aoc-1-2020/target/debug/deps/aoc_1_2020.d: src/main.rs
+
+src/main.rs:
