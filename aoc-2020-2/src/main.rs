498b06418ddf4d23381ee2a02f5f442f6c5a40f1
heroichornet <schnipper@gmail.com>
2021-11-21 18:49:24 +0100

    part to aoc 2020 day 2

--- aoc-2020-2/src/main.rs
+++ aoc-2020-2/src/main.rs
@@ -1,50 +1,74 @@
 fn main() {
-    let answer = INPUT.lines()
-        .filter(|&x| check_line(x))
+    let answer1 = INPUT.lines()
+        .filter(|&x| Entry::new(x).check_sled_rental())
         .count();
 
-    println!("{}",answer);
-}
-
-fn check_line(line:&str) -> bool {
-
-    let mut s:Vec<&str> = line
-        .split(' ')
-        .collect();
-
-    let range = s
-        .remove(0)
-        .split_once('-')
-        .unwrap();
-
-    let min:usize = range
-        .0
-        .parse()
-        .unwrap();
-
-    let max:usize = range
-        .1
-        .parse()
-        .unwrap();
-
-    let letter = s
-        .remove(0)
-        .trim_end_matches(":")
-        .chars()
-        .nth(0)
-        .unwrap();
-
-    let password = s.remove(0);
-
-    let num = password
-        .chars()
-        .filter(|c| c.eq(&letter))
+    let answer2 = INPUT.lines()
+        .filter(|&x| Entry::new(x).check_toboggan_rental())
         .count();
 
-
-    num >= min && num <= max
-
+    println!("answer1:{} answer2:{}",answer1,answer2);
 }
+
+struct Entry {
+    left:usize,
+    right:usize,
+    letter:char,
+    password:String,
+}
+
+impl Entry{
+
+    fn check_toboggan_rental(&self) -> bool {
+        self.password.chars().nth(self.left - 1).unwrap().eq(&self.letter) ^
+            self.password.chars().nth(self.right -1 ).unwrap().eq(&self.letter)
+    }
+
+    fn check_sled_rental(&self) -> bool {
+
+        let num = self.password
+            .chars()
+            .filter(|c| c.eq(&self.letter))
+            .count();
+
+        num >= self.left && num <= self.right
+    }
+
+    fn new(line:&str) -> Entry{
+
+        let mut s:Vec<&str> = line
+            .split(' ')
+            .collect();
+
+        let range = s
+            .remove(0)
+            .split_once('-')
+            .unwrap();
+
+        let min:usize = range
+            .0
+            .parse()
+            .unwrap();
+
+        let max:usize = range
+            .1
+            .parse()
+            .unwrap();
+
+        let letter = s
+            .remove(0)
+            .trim_end_matches(":")
+            .chars()
+            .nth(0)
+            .unwrap();
+
+        let password = s.remove(0);
+
+        Entry{ left: min, right: max, letter, password: String::from(password) }
+    }
+}
+
+
 
 const INPUT:&str = "1-3 a: abcde
 1-3 b: cdefg
