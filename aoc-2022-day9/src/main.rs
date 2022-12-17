2257b955b750c87b8bd7f165b9dd9bb3cf787393
heroichornet <schnipper@gmail.com>
2022-12-17 23:27:14 +0100

    aoc day 9 done

--- aoc-2022-day9/src/main.rs
+++ aoc-2022-day9/src/main.rs
@@ -1,21 +1,32 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, borrow::{Borrow, BorrowMut}};
 
 fn main() {
-
    
-    let mut rope = Rope{
-        tail_history: HashSet::new(),
-        head: Position{x: 0, y: 0},
-        tail: Position{x: 0, y: 0},
+    let mut rope1 = Rope{
+        history: HashSet::new(),
+        positions: vec![Position{x: 0, y: 0};10],
     };
 
     include_str!("../input.txt")
             .lines()
             .map(parse_steps)
-            .map(|x| rope.move_rope(x.unwrap()))
+            .map(|x| rope1.move_rope(x.unwrap()))
             .for_each(drop);
     
-    println!("{}",rope.tail_history.len());
+    println!("Length of rope 1 {}",rope1.history.len());
+
+    let mut rope2 = Rope{
+        history: HashSet::new(),
+        positions: vec![Position{x: 0, y: 0};2],
+    };
+
+    include_str!("../input.txt")
+            .lines()
+            .map(parse_steps)
+            .map(|x| rope2.move_rope(x.unwrap()))
+            .for_each(drop);
+    
+    println!("Length of rope 2{}",rope2.history.len());
 
 }
 
@@ -45,9 +56,23 @@ struct Position {
 }
 #[derive(Debug, PartialEq,Eq,Clone)]
 struct Rope {
-    tail_history: HashSet<Position>,
-    head: Position,
-    tail: Position,
+    history: HashSet<Position>,
+    positions: Vec<Position>,
+}
+
+impl Position {
+
+    fn get_distance(a:&Position,b:&Position) -> (i64,i64)  {
+        (
+            (a.x- b.x).abs() , 
+            (a.y- b.y).abs()
+        )
+    }
+
+    fn head_is_not_touching_tail(a:&Position,b:&Position) -> bool {
+        let (delta_x, delta_y) = Self::get_distance(a,b);
+        delta_x > 1 || delta_y > 1
+    }
 }
 
 impl Rope {
@@ -55,41 +80,51 @@ impl Rope {
     fn move_rope(&mut self, steps: Vec<Step>) {
 
         for step in steps {
-            match step.direction {
-                Direction::Up => self.head.y += 1,
-                Direction::Down => self.head.y -= 1,
-                Direction::Left => self.head.x -= 1,
-                Direction::Right => self.head.x += 1,
-            }
-            if self.head_is_not_touching_tail() {
-                let (delta_x, delta_y) = self.get_distance();
 
-                if delta_x >= 1 {
-                    self.tail.x += (self.head.x- self.tail.x).clamp(-1,1);
-                }
-                if delta_y >= 1 {
-                    self.tail.y += (self.head.y - self.tail.y).clamp(-1,1);
-                }
-                self.tail_history.insert(self.tail.clone());
+            let mut head = self.positions.first().unwrap().clone();
+
+            match step.direction {
+                Direction::Up => head.y += 1,
+                Direction::Down => head.y -= 1,
+                Direction::Left => head.x -= 1,
+                Direction::Right => head.x += 1,
             }
+            self.positions[0]=head;
+
+            self.positions.iter()
+                          .enumerate()
+                          .map(|i| (i.0,i.0+1))
+                          .collect::<Vec<(_,_)>>()
+                          .iter()
+                          .for_each(|(a,b)| {
+                                let head = self.positions.get(*a).unwrap();
+                                if let Some(tail) = self.positions.get(*b){
+                                    
+                                    let mut tail = tail.clone();
+                                    if Position::head_is_not_touching_tail(&head,&tail) {
+                        
+                                        let (delta_x, delta_y) = Position::get_distance(&head,&tail);
+                    
+                                        if delta_x >= 1 {
+                                            tail.x += (head.x - tail.x).clamp(-1,1);
+                                        }
+                                        if delta_y >= 1 {
+                                            tail.y += (head.y - tail.y).clamp(-1,1);
+                                        }
+                                    }
+                                    self.positions[*b] = tail;
+                                }
+
+                          });
+            self.history.insert(self.positions.last().unwrap().clone());
         }
+        
+    }
 
         
 
-    }
-
-    fn get_distance(&self) -> (i64,i64)  {
-        (
-            (self.head.x- self.tail.x).abs() , 
-            (self.head.y- self.tail.y).abs()
-        )
-    }
-
-    fn head_is_not_touching_tail(&self) -> bool {
-        let (delta_x, delta_y) = self.get_distance();
-        delta_x > 1 || delta_y > 1
-    }
 }
+
 
 fn parse_steps(input:&str) -> Option<Vec<Step>> {
 
@@ -109,17 +144,14 @@ fn parse_steps(input:&str) -> Option<Vec<Step>> {
 #[cfg(test)]
 mod tests {
 
-    use std::hash::Hash;
-
     use super::*;
 
     #[test]
     fn motion_parsing() {
 
         let _rope = Rope{
-            tail_history: HashSet::new(),
-            head: Position{x: 0, y: 0},
-            tail: Position{x: 0, y: 0},
+            history: HashSet::new(),
+            positions: vec![Position{x: 0, y: 0};2],
         };
         
         assert_eq!(parse_steps("D 142"), Some(vec![Step{direction: Direction::Down}; 142]));
@@ -130,31 +162,33 @@ mod tests {
     fn rope_movement() {
 
         let mut rope = Rope{
-            tail_history: HashSet::new(),
-            head: Position{x: 0, y: 0},
-            tail: Position{x: 0, y: 0},
+            history: HashSet::new(),
+            positions: vec![Position{x: 0, y: 0};2],
         };
 
         rope.move_rope(parse_steps("R 1").unwrap());
-        assert_eq!(rope.head, Position{x: 1, y: 0});
-        assert_eq!(rope.tail, Position{x: 0, y: 0});
+        assert_eq!(*rope.positions.first().unwrap(), Position{x: 1, y: 0});
+        assert_eq!(*rope.positions.last().unwrap(), Position{x: 0, y: 0});
 
         rope.move_rope(parse_steps("R 1").unwrap());
-        assert_eq!(rope.head, Position{x: 2, y: 0});
-        assert_eq!(rope.tail, Position{x: 1, y: 0});
+        assert_eq!(*rope.positions.first().unwrap(), Position{x:2, y: 0});
+        assert_eq!(*rope.positions.last().unwrap(), Position{x: 1, y: 0});
 
         rope.move_rope(parse_steps("R 1").unwrap());
-        assert_eq!(rope.head, Position{x: 3, y: 0});
-        assert_eq!(rope.tail, Position{x: 2, y: 0}); 
+        assert_eq!(*rope.positions.first().unwrap(), Position{x: 3, y: 0});
+        assert_eq!(*rope.positions.last().unwrap(), Position{x: 2, y: 0});
 
         rope.move_rope(parse_steps("R 1").unwrap());
-        assert_eq!(rope.head, Position{x: 4, y: 0});
-        assert_eq!(rope.tail, Position{x: 3, y: 0});
+        assert_eq!(*rope.positions.first().unwrap(), Position{x: 4, y: 0});
+        assert_eq!(*rope.positions.last().unwrap(), Position{x: 3, y: 0});
 
         rope.move_rope(parse_steps("U 1").unwrap());
-        assert_eq!(rope.head, Position{x: 4, y: 1});
-        assert_eq!(rope.tail, Position{x: 3, y: 0});
+        assert_eq!(*rope.positions.first().unwrap(), Position{x:4, y: 1});
+        assert_eq!(*rope.positions.last().unwrap(), Position{x: 3, y: 0});
 
+        rope.move_rope(parse_steps("U 1").unwrap());
+        assert_eq!(*rope.positions.first().unwrap(), Position{x:4, y: 2});
+        assert_eq!(*rope.positions.last().unwrap(), Position{x: 4, y: 1});
 
     }
     
@@ -162,9 +196,8 @@ mod tests {
     fn test_move_counter(){
 
         let mut rope = Rope{
-            tail_history: HashSet::new(),
-            head: Position{x: 0, y: 0},
-            tail: Position{x: 0, y: 0},
+            history: HashSet::new(),
+            positions: vec![Position{x: 0, y: 0};2],
         };
         
         rope.move_rope(parse_steps("R 4").unwrap());
@@ -176,30 +209,18 @@ mod tests {
         rope.move_rope(parse_steps("L 5").unwrap());
         rope.move_rope(parse_steps("R 2").unwrap());
 
-        assert_eq!(rope.tail_history.len(),13);
+        assert_eq!(rope.history.len(),13);
 
     } 
 
     #[test]
     fn test_head_is_not_touching(){
-        let mut rope = Rope{
-            tail_history: HashSet::new(),
-            head: Position{x: 0, y: 0},
-            tail: Position{x: 0, y: 0},
-        };
 
-        assert_eq!(rope.head_is_not_touching_tail(), false);
-        rope.tail = Position{x: 1, y: 0};
-        assert_eq!(rope.head_is_not_touching_tail(), false);
-        rope.tail = Position{x: 0, y: 1};
-        assert_eq!(rope.head_is_not_touching_tail(), false);
-        rope.tail = Position{x: 1, y: 1};
-        assert_eq!(rope.head_is_not_touching_tail(), false);
-        rope.tail = Position{x: 2, y: 0};
-        assert_eq!(rope.head_is_not_touching_tail(), true);
-        rope.tail = Position{x: 0, y: 2};
-        assert_eq!(rope.head_is_not_touching_tail(), true);
-
+        assert_eq!(Position::head_is_not_touching_tail(&Position{x:0,y:0}, &Position{x:0,y:0}),false);
+        assert_eq!(Position::head_is_not_touching_tail(&Position{x:1,y:0}, &Position{x:0,y:0}),false);
+        assert_eq!(Position::head_is_not_touching_tail(&Position{x:0,y:1}, &Position{x:0,y:0}),false);
+        assert_eq!(Position::head_is_not_touching_tail(&Position{x:1,y:1}, &Position{x:0,y:0}),false);
+        assert_eq!(Position::head_is_not_touching_tail(&Position{x:2,y:0}, &Position{x:0,y:0}),true);
 
     }
 
@@ -207,9 +228,8 @@ mod tests {
     fn test_get_distance(){
 
         let mut rope = Rope{
-            tail_history: HashSet::new(),
-            head: Position{x: 0, y: 0},
-            tail: Position{x: 0, y: 0},
+            history: HashSet::new(),
+            positions: vec![Position{x: 0, y: 0};2],
         };
     
         include_str!("../test_input.txt").lines()
@@ -217,7 +237,7 @@ mod tests {
              .map(|x| rope.move_rope(x.unwrap()))
              .for_each(drop);
         
-        assert_eq!(rope.tail_history.len(), 13);
+        assert_eq!(rope.history.len(), 13);
     
     }
 }
