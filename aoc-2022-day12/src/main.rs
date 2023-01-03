5bbc15d0910cb23f81d1145f76d0a9f89a1ca391
heroichornet <schnipper@gmail.com>
2023-01-03 23:28:35 +0100

    fixed day 12 aoc

--- aoc-2022-day12/src/main.rs
+++ aoc-2022-day12/src/main.rs
@@ -4,8 +4,12 @@ use std::collections::{
     };
 
 fn main() {
-    let output = find_shortest_path(include_str!("../input.txt"));
-    println!("output: {}", output);
+    let output = find_shortest_path(include_str!("../input.txt"),'S');
+    println!("Ans1: {:?}", output);
+
+    let mut output = find_shortest_path(include_str!("../input.txt"),'a');
+    output.sort();
+    println!("Ans2: {:?}", output.first());
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
@@ -14,95 +18,104 @@ struct Point {
     y: usize,
 }
 
-fn find_shortest_path(input: &str) -> String {
+fn find_shortest_path(input: &str,starting_char:char) -> Vec<u64> {
     
     let mut grid: Vec<i64> = vec![];
     let row_size: usize = input.lines().next().unwrap().len();
     let col_size: usize = input.lines().count();
-    let mut starting_point:usize = 0;
+    let mut starting_points =vec![];
     for line in input.lines() {
         for c in line.trim().chars() {
             match c {
-                'a'..='z' => grid.push(c as i64),
-                'S'  => {
-                        starting_point = grid.len();
+                _ if c == starting_char || c == 'S' => {
+                    starting_points.push(grid.len());
+                    if c == 'S' {
                         grid.push(('a' as u8 - 1) as i64)
-                    },
+                    }else{
+                        grid.push(c as i64) 
+                    }
+                },
+                'a'..='z' => grid.push(c as i64),
                 'E'  => grid.push(('z' as u8 + 1) as i64),
                 _ => (),
             }
         }
     }
 
-    let starting_point = Point { x: starting_point%row_size, y: starting_point/row_size };
-    let mut queue: VecDeque<Point> = VecDeque::new();
-    queue.push_back(starting_point);
-    let mut visited = HashSet::new();
-    let mut current_depth:usize = 0;
+    let mut depths:Vec<u64> = vec![];
 
-    while !queue.is_empty(){
+    for starting_point in starting_points {
 
-        let mut level_size:usize = queue.len();
-        
+        let starting_point = Point { x: starting_point%row_size, y: starting_point/row_size };
+        let mut queue: VecDeque<Point> = VecDeque::new();
+        queue.push_back(starting_point);
+        let mut visited = HashSet::new();
+        let mut current_depth:usize = 0;
+        let mut finished = false;
 
-        while level_size > 0 {
+        while !queue.is_empty() && !finished {
 
-            level_size -= 1;
+            let mut level_size:usize = queue.len();
 
-            let current_point = queue.pop_front().unwrap();
-            let current_index = current_point.x + current_point.y*row_size;
-            let current_char = grid[current_index];
+            while level_size > 0 {
 
-            if current_char == ('z' as u8+1) as i64 {
-                println!("done!");
-                return current_depth.to_string();
-            } else  {
+                level_size -= 1;
 
-                visited.insert(current_index);
-                
-                if current_point.x > 0 && (grid[current_point.x-1 + current_point.y*row_size]-current_char) <=1 {
-                    if !visited.contains(&(current_point.x-1 + current_point.y*row_size)) {
-                        let new_point = Point { x: current_point.x - 1, y: current_point.y };
-                        if !queue.contains(&new_point){
-                            queue.push_back(new_point);
+                let current_point = queue.pop_front().unwrap();
+                let current_index = current_point.x + current_point.y*row_size;
+                let current_char = grid[current_index];
+
+                if current_char == ('z' as u8+1) as i64 {
+                    depths.push(current_depth as u64);
+                    finished = true;
+                } else  {
+
+                    visited.insert(current_index);
+                    
+                    if current_point.x > 0 && (grid[current_point.x-1 + current_point.y*row_size]-current_char) <=1 {
+                        if !visited.contains(&(current_point.x-1 + current_point.y*row_size)) {
+                            let new_point = Point { x: current_point.x - 1, y: current_point.y };
+                            if !queue.contains(&new_point){
+                                queue.push_back(new_point);
+                            }
                         }
                     }
-                }
 
-                if current_point.x < row_size-1 && (grid[current_point.x+1 + current_point.y*row_size]-current_char) <= 1 {
-                    if !visited.contains(&(current_point.x+1 + current_point.y*row_size)) {
-                        let new_point = Point { x: current_point.x + 1, y: current_point.y };
-                        if !queue.contains(&new_point){
-                            queue.push_back(new_point);
+                    if current_point.x < row_size - 1 && (grid[current_point.x+1 + current_point.y*row_size]-current_char) <= 1 {
+                        if !visited.contains(&(current_point.x+1 + current_point.y*row_size)) {
+                            let new_point = Point { x: current_point.x + 1, y: current_point.y };
+                            if !queue.contains(&new_point){
+                                queue.push_back(new_point);
+                            }
                         }
                     }
-                }
 
-                if current_point.y > 0 && (grid[current_point.x + (current_point.y-1)*row_size]-current_char) <= 1{
-                    if !visited.contains(&(current_point.x + (current_point.y-1)*row_size)) {
-                        let new_point = Point { x: current_point.x, y: current_point.y - 1 };
-                        if !queue.contains(&new_point){
-                            queue.push_back(new_point);
+                    if current_point.y > 0 && (grid[current_point.x + (current_point.y-1)*row_size]-current_char) <= 1{
+                        if !visited.contains(&(current_point.x + (current_point.y-1)*row_size)) {
+                            let new_point = Point { x: current_point.x, y: current_point.y - 1 };
+                            if !queue.contains(&new_point){
+                                queue.push_back(new_point);
+                            }
                         }
                     }
-                }
 
-                if current_point.y < col_size-1 && (grid[current_point.x+(current_point.y+1)*row_size]-current_char) <= 1 {
-                    if !visited.contains(&(current_point.x+(current_point.y+1)*row_size)) {
-                        let new_point = Point { x: current_point.x, y: current_point.y + 1 };
-                        if !queue.contains(&new_point){
-                            queue.push_back(new_point);
+                    if current_point.y < col_size - 1 && (grid[current_point.x+(current_point.y+1)*row_size]-current_char) <= 1 {
+                        if !visited.contains(&(current_point.x+(current_point.y+1)*row_size)) {
+                            let new_point = Point { x: current_point.x, y: current_point.y + 1 };
+                            if !queue.contains(&new_point){
+                                queue.push_back(new_point);
+                            }
                         }
                     }
-                }
-            }   
+                }   
+            }
+
+            current_depth+=1;
+
         }
-
-        current_depth+=1;
-
     }
 
-    current_depth.to_string()
+    depths
 }
 
 
@@ -119,8 +132,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let output = find_shortest_path(INPUT);
-        assert_eq!(output, "31");
+        let output = find_shortest_path(INPUT,'S');
+        assert_eq!(output, vec![31]);
     }
 
 }
